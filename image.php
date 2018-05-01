b6e4fe6753bed9153efbd5d2ff5f6c0fd3db5814
rb2010 <ranjanbhandari@gmail.com>
2018-05-01 14:48:50 -0400

    Update image.php

--- image.php
+++ image.php
@@ -3,8 +3,7 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
-#$deployment = getenv("COLOR");
-$deployment = 'green'
+$deployment = 'green';
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
